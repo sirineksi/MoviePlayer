9c382a000d5bb910e50a7903343e2376c0f10665
Şirin Ekşi <sirin.eksi@hotmail.com>
2023-08-28 14:30:58 +0300

    Initial Commit

---
+++ MoviePlayer/MoviePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviePlayerApp.swift
+//  MoviePlayer
+//
+//  Created by ceksi on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
